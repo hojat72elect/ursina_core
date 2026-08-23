d9a4755e4792458e7985c8266204bd10ee61d7af
pokepetter <pokepetter@gmail.com>
2026-08-23 21:04:47 +0200

    fixed issue with Cone caching breaking if you destroyed the mesh.

--- ursina/models/procedural/cone.py
+++ ursina/models/procedural/cone.py
@@ -6,10 +6,13 @@ class Cone(Mesh):
     _cache = {}
     def __new__(cls, resolution=4, radius=.5, height=1, add_bottom=True, mode='triangle'):
         key = (resolution, radius, height, add_bottom, mode)
-        if key in cls._cache:
+        cached_mesh = cls._cache.get(key, None)
+        if cached_mesh:
             return deepcopy(cls._cache[key])
 
         instance = super().__new__(cls)
+
+        instance.eternal = True
         cls._cache[key] = instance
         return instance
 
@@ -38,9 +41,10 @@ class Cone(Mesh):
 
 
 if __name__ == '__main__':
-    from ursina import Ursina, Entity, color, EditorCamera
+    from ursina import Ursina, Entity, color, EditorCamera, destroy, scene
     app = Ursina()
-    e = Entity(model=Cone(3), texture='brick')
+    # e = Entity(model=Cone(3), texture='brick')
+    graphics = Entity(model=Cone(8, radius=.4, height=2), origin_y=-.5, color=color.hex('#121024'))
 
     # # rotate model
     # for i, v in enumerate(e.model.vertices):
@@ -52,4 +56,14 @@ if __name__ == '__main__':
     Entity(model='wireframe_cube')
     origin = Entity(model='quad', color=color.orange, scale=(.05, .05))
     ed = EditorCamera()
+    def input(key):
+        global graphics
+        if key == 'd':
+            destroy(graphics)
+            graphics.model = None
+        if key == 'space':
+            graphics = Entity(model=Cone(8, radius=.4, height=2), origin_y=-.5, color=color.hex('#121024'))
+
+
     app.run()
+
