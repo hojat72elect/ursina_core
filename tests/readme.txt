ed8d69a9487121b0f5ad8fcf9ed12d697f022fd3
hojat72elect <hojat72elect@gmail.com>
2026-09-03 05:13:15 -0400

    Moved all tests to my own game engine.

--- tests/readme.txt
+++
@@ -1,2 +0,0 @@
-Most modules has test in the bottom of the file.
-This folder is for additional tests that doesn't make sense to put there.
--- tests/test_load_blender_model/test_load_blender_model.py
+++
@@ -1,9 +0,0 @@
-from ursina import *
-
-
-app = Ursina(borderless=False)
-
-Entity(model='blender_test_model', collider='mesh')
-EditorCamera()
-
-app.run()
